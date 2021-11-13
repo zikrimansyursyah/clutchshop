c186d88e88b6daa2dfb26f3e71dff31c453c9c31
Deni-Prasetyo <prasetyodenibudi@gmail.com>
2021-11-14 02:41:51 +0700

    menambahkan login

--- main.js
+++ main.js
@@ -2,14 +2,22 @@ let slideIndex = 0;
 showSlides();
 
 function showSlides() {
-    let slides = document.getElementsByClassName("slider");
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1
-    }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 4000);
+  let slides = document.getElementsByClassName("slider");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 4000);
 }
+
+document.getElementById("button").addEventListener("click", function () {
+  document.querySelector(".popup").style.display = "flex";
+});
+
+document.querySelector(".close").addEventListener("click", function () {
+  document.querySelector(".popup").style.display = "none";
+});
